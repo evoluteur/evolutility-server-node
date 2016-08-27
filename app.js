caf94141bbdca33ca10b86d5158fc4883006404a
Olivier Giulieri <ogiulieri@gmail.com>
2016-08-26 21:48:15 -0700

    Upgraded dependencies + removed unused ones + renamed config option "port" to "apiPort".

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
@@ -8,13 +7,6 @@ var routes = require('./js/routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'client/views'));
-app.set('view engine', 'jade');
-////app.set('view engine', 'html');
-/**/
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
 	apiPath: '/api/v1/evolutility/',
-	connectionString: process.env.DATABASE_URL || 'postgres://evol:love@localhost:5432/evolutility',
+	apiPort: 3000,
+	connectionString: process.env.DATABASE_URL || 'postgres://evol:love@localhost:5432/Evolutility',
 	schema: 'evol_demo',
-	port: 3000,
 	consoleLog: true
 };
