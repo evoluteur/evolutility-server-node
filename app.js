08949954ed15b1d017ec59f485bc7fa88efe9cee
Olivier Giulieri <ogiulieri@gmail.com>
2016-12-23 22:06:35 -0800

    Dependencies updates.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var routes = require('./js/routes');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './client', 'public')));
