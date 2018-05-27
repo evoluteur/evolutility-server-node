4355b52f52b21c559b469f67c6527ad930913567
evoluteur <ogiulieri@gmail.com>
2018-05-27 15:49:38 -0700

    Cleanup: renamed and split files.

--- js/utils/crud.js
+++ js/utils/crud.js
@@ -1,6 +1,6 @@
 /*! *******************************************************
  *
- * evolutility-server-node :: utils/orm.js
+ * evolutility-server-node :: utils/crud.js
  *
  * https://github.com/evoluteur/evolutility-server-node
  * (c) 2018 Olivier Giulieri
@@ -215,58 +215,6 @@ function getMany(req, res) {
     }
 }
 
-
-// --------------------------------------------------------------------------------------
-// -----------------    GET CHARTS   ----------------------------------------------------
-// --------------------------------------------------------------------------------------
-
-// - returns data for a single charts
-function chartField(req, res) {
-    logger.logReq('GET CHART', req);
-
-    var m = dico.getModel(req.params.entity),
-        fid = req.params.field,
-        sqlParams = [],
-        sql;
-    var sqlCount = 'count(*)::integer AS value';
-
-    if(m && fid){
-        var f = m.fieldsH[fid];
-        if(f){
-            var col = '"'+f.column+'"',
-                sqlFrom = ' FROM '+m.schemaTable+' AS t1';
-            if(f.type==='lov' && f.lovtable){
-                var clov = f.lovcolumn||'name';
-
-                sql='SELECT t2.id, t2.'+clov+'::text AS label, '+sqlCount+
-                    sqlFrom+
-                    ' LEFT JOIN '+schema+'."'+f.lovtable+'" AS t2'+
-                        ' ON t1.'+col+'=t2.id'+
-                    ' GROUP BY t2.id, t2.'+clov;
-            }else if(f.type==='boolean'){
-                var cId = 'CASE '+col+' WHEN true THEN 1 ELSE 0 END',
-                    cLabel = 'CASE '+col+' WHEN true THEN \'Yes\' ELSE \'No\' END';
-
-                sql='SELECT '+cId+'::integer AS id, '+
-                        cLabel+'::text AS label, '+sqlCount+
-                    sqlFrom+
-                    ' GROUP BY '+cId+','+cLabel;
-            }else{ // TODO: bukets
-                sql='SELECT '+col+'::text AS label, '+sqlCount+
-                    sqlFrom+
-                    ' GROUP BY '+col;
-            }
-            sql += ' ORDER BY label ASC'+
-                   ' LIMIT '+defaultPageSize+';';
-
-            query.runQuery(pool, res, sql, sqlParams, false);
-        }
-    }else{
-        return res.json(logger.errorMsg('Invalid entity or field.', 'chartField'));
-    }
-}
-
-
 // --------------------------------------------------------------------------------------
 // -----------------    GET ONE   -------------------------------------------------------
 // --------------------------------------------------------------------------------------
@@ -446,9 +394,6 @@ module.exports = {
 
     // - Sub-collections
     getCollec: collecOne,
-
-    // - Charts
-    chartField: chartField,
 
     // - LOVs (for dropdowns)
     lovOne: lovOne
