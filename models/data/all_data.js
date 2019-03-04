809d8384e028b25adc7c4991fd43c184de35a7f1
evoluteur <ogiulieri@gmail.com>
2019-03-04 00:50:01 -0800

    Moved PIM demo data to directory.

--- models/data/all_data.js
+++ models/data/all_data.js
@@ -1,14 +1,15 @@
 
 module.exports = {
-	todo: require('./todo.data'),
-	contact: require('./contact.data'),
-	comics: require('./comics.data'),
-	winecellar: require('./winecellar.data'),
-	winetasting: require('./winetasting.data'),
-	restaurant: require('./restaurant.data'),
-	test: require('./test/test.data'),
-
+	todo: require('./pim/todo-data'),
+	contact: require('./pim/contact-data'),
+	comics: require('./pim/comics-data'),
+	winecellar: require('./pim/winecellar-data'),
+	winetasting: require('./pim/winetasting-data'),
+	restaurant: require('./pim/restaurant-data'),
+	
 	album: require('./music/album-data'),
 	artist: require('./music/artist-data'),
 	track: require('./music/track-data'),
+
+	test: require('./test/test-data'),
 };
