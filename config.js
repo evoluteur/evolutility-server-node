9ea3ea7e1bffef3b1b6e114a012a7401a5988d4c
evoluteur <ogiulieri@gmail.com>
2019-11-27 19:33:09 -0800

    schemaQueries false + more comments.

--- config.js
+++ config.js
@@ -1,24 +1,38 @@
+/*
+  Evolutility-Server-Node configuration file
+*/
 
 module.exports = {
 
-	// Path to REST API
+	// - Path to REST API
 	apiPath: '/api/v1/',
 	apiPort: process.env.PORT || 2000,
 
-	graphQL: true,
-	schemaQueries: true,
-
-	// DB connection
+	// - DB connection
 	connectionString: process.env.DATABASE_URL || 'postgres://evol:love@localhost:5432/Evolutility', 
 	schema: 'evolutility',
 
-	// OPTIONS
-	// Pagination and maximum number of rows
+	// - GraphQL support
+	graphQL: true,
+
+	// - Permission to query DB schema for list of tables and columns
+	schemaQueries: false,
+
+	// - API discovery
+	apiInfo: true,
+
+	// - Pagination and maximum number of rows
 	pageSize: 50,
 	lovSize: 100,
+
+	// - CSV Export
 	csvSize: 1000,
 	csvHeader: 'id', //label', // possible values: id, label
 
+	// - Directory for uploaded files
+	uploadPath: '../evolutility-ui-react/public/pix/',
+
+	// - Optional fields
 	// - Timestamp columns u_date and c_date w/ date of record creation and last update 
 	wTimestamp: true,
 	// - "WhoIs" columns u_uid and c_uid w/ userid of creator and last modifier
@@ -26,18 +40,11 @@ module.exports = {
 	// - Comments & Ratings (community feature) 
 	wComments: false,
 	wRating: false,
+	// - Columns containing created and last updated dates
+	createdDateColumn: 'c_date',
+	updatedDateColumn: 'u_date',
 
-    // - Columns containing created and last updated dates
-    createdDateColumn: 'c_date',
-    updatedDateColumn: 'u_date',
-
-	// - API discovery
-	apiInfo: true,
-
-	// Directory for uploaded files
-	uploadPath: '../evolutility-ui-react/public/pix/',
-
-	// Logs
+	// - Logs (to file and console)
 	consoleLog: true,
 	fileLog: false,
 
--- models/all_models.js
+++ models/all_models.js
@@ -8,20 +8,20 @@ https://github.com/evoluteur/evolutility-models
 */
 
 module.exports = {
+  // --- Organizer
   todo: require('./organizer/todo'),
   contact: require('./organizer/contact'),
   comics: require('./organizer/comics'),
   restaurant: require('./organizer/restaurant'),
   winecellar: require('./organizer/winecellar'),
   winetasting: require('./organizer/winetasting'),
-
+  // --- Music
   album: require('./music/album'),
   artist: require('./music/artist'),
   track: require('./music/track'),
-
+  // --- Tests
   test: require('./tests/test'),
-  
-  
+  // -- Designer
   world: require('./designer/world'),
   object: require('./designer/object'),
   field: require('./designer/field'),
