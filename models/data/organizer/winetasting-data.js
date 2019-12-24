b3ca775ca7f7ecce58c722ea10c1471137def805
evoluteur <ogiulieri@gmail.com>
2019-12-23 23:59:26 -0800

    Oops, forgot to change column name.

--- models/data/organizer/winetasting-data.js
+++ models/data/organizer/winetasting-data.js
@@ -1,6 +1,6 @@
 module.exports = [
     {
-        wine_id: 1,
+        wine: 1,
         drink_date: "2018-05-08",
         robe: "light",
         nose: "fruity",
@@ -8,7 +8,7 @@ module.exports = [
         notes: "too young"
     },
     {
-        wine_id: 1,
+        wine: 1,
         drink_date: "2018-07-10",
         robe: "light",
         nose: "light fruit",
@@ -16,7 +16,7 @@ module.exports = [
         notes: ""
     },
     {
-        wine_id: 1,
+        wine: 1,
         drink_date: "2019-01-10",
         robe: "light",
         nose: "light fruit",
@@ -24,7 +24,7 @@ module.exports = [
         notes: ""
     },
     {
-        wine_id: 1,
+        wine: 1,
         drink_date: "2018-12-12",
         robe: "light",
         nose: "light fruit",
@@ -32,21 +32,21 @@ module.exports = [
         notes: "Great"
     },
     {
-        wine_id: 2,
+        wine: 2,
         drink_date: "2018-04-23",
         robe: "golden",
         nose: "fruity",
         taste: "incredible"
     },
     {
-        wine_id: 2,
+        wine: 2,
         drink_date: "2018-05-12",
         robe: "clear",
         nose: "strong and sweet",
         taste: "outstanding"
     },
     {
-        wine_id: 3,
+        wine: 3,
         drink_date: "2018-01-12",
         robe: "thick",
         nose: "strong",
@@ -54,41 +54,41 @@ module.exports = [
         notes:"Great w/ beef."
     },
     {
-        wine_id: 4,
+        wine: 4,
         drink_date: "2018-05-05",
         robe: "light",
         nose: "strong",
         taste: "good"
     },
     {
-        wine_id: 4,
+        wine: 4,
         drink_date: "2017-08-05",
         robe: "light",
         nose: "strong",
         taste: "good"
     },
     {
-        wine_id: 4,
+        wine: 4,
         drink_date: "2018-08-18",
         robe: "light",
         nose: "strong",
         taste: "ok"
     },
     {
-        wine_id: 5,
+        wine: 5,
         drink_date: "2017-05-05",
         nose: "strong+",
         taste: "excellent"
     },
     {
-        wine_id: 5,
+        wine: 5,
         drink_date: "2018-10-22",
         robe: "rich",
         nose: "strong",
         taste: "very good"
     },
     {
-        wine_id: 5,
+        wine: 5,
         drink_date: "2019-02-02",
         robe: "rich",
         nose: "strong",
